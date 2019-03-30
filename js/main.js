77764008502e60c21ab1de31eeef77d62a33fea2
Alexander Watts <alexcharleswatts@gmail.com>
2019-03-30 22:17:02 +0000

    Defined request headers, made a template fetch function and fetched the projects

--- js/main.js
+++ js/main.js
@@ -5,3 +5,41 @@
  */
 
 var url = 'https://api.github.com/';
+
+/**
+ * Set headers for authentication
+ * and to define the media type
+ */
+
+var headers = new Headers();
+headers.append('Authorization', `Basic ${KEY}`);
+headers.append('Accept', 'application/vnd.github.inertia-preview+json'); 
+
+/**
+ * Define a template for fetching data
+ * this makes the code more reusable.
+ * Accepts a url and parses the response
+ * as JSON.
+ */
+
+function fetchData(url) {
+
+	return fetch(url, {headers:headers}).then(response => {
+		return response.json();
+	});
+
+}
+
+/**
+ * Fetch the data for each of CoInvestors
+ * projects
+ */
+function fetchProjects(url) {
+
+	fetchData(url).then(res => {
+		console.log(res);
+	});
+
+}
+
+fetchProjects(url + 'repos/CoInvestor/webtest-2019/projects');
