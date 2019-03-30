a772459826a3a367674a0306f82d24cb10c6a451
Alexander Watts <alexcharleswatts@gmail.com>
2019-03-30 21:52:28 +0000

    Added API key to project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+/**
+ * Goal:
+ * Use the GitHub API to create a web page that combines 
+ * all issues from CoInvestor's GitHub Project Boards.
+ */
+
+var url = 'https://api.github.com/';
