233bf9710e3d8a3257b8ff2d90ba2085dbcc3350
Alexander Watts <alexcharleswatts@gmail.com>
2019-03-30 23:42:35 +0000

    Created function fetchCards that gets data about each card. Added a function that creates a card and places it into a column.

--- js/main.js
+++ js/main.js
@@ -31,6 +31,39 @@ function fetchData(url) {
 }
 
 /**
+ * Format the column name returned from the
+ * API removing the whitespace and making
+ * every character lowercase.
+ * i.e In Progress to inprogress
+ * This will determine which column a card
+ * will go in matching the id name specified
+ * in the HTML.
+ */
+function format(column_name) {
+	return column_name.replace(/\s/g, '').toLowerCase();
+}
+
+/**
+ * Create a card to be displayed on the project board
+ */
+function createCard(description, project_name, creator, creator_profile, date_created, column_name) {
+
+	//Create card
+	var card = `<div class="card">
+		<h2>${description}</h2>
+		<p>From project board: ${project_name}<p>
+		<p><a href="${creator_profile}">${creator}</a><span class="date"> - ${date_created}</span></p>
+	</div>`;
+
+	//Get the correct column
+	var column = document.getElementById(`${column_name}`);
+
+	//Add card to the column
+	column.innerHTML += card;
+
+}
+
+/**
  * Fetch the data for each of CoInvestors
  * projects
  */
@@ -56,13 +89,35 @@ function fetchColumns(project_id, project_name) {
 	fetchData(url + `projects/${project_id}/columns`).then(response => {
 
 		//Get the projects column id and the name of 
-		//the column it's in.
+		//the column it's in. Send the results to fetchCards
+		//function
 		response.forEach(element => {
-			console.log(element.id);
-			console.log(element.name);
+			fetchCards(element.id, format(element.name), project_name);
 		});
 
 	});
+}
+
+/**
+ * Fetch the cards in each column
+ */
+function fetchCards(column_id, column_name, project_name) {
+
+	fetchData(url + `projects/columns/${column_id}/cards`).then(response => {
+
+		//Get the card data and create a card adding it
+		//to the HTML
+		response.forEach(element => {
+			createCard(element.note,
+				project_name,  
+				element.creator.login, 
+				element.creator.html_url, 
+				element.created_at, 
+				column_name);
+		});
+
+	});
+
 }
 
 fetchProjects();
